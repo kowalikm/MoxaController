a68f0af230bcb2e6fd58a2da3834f8fd93d07e53
Mateusz Kowalik <kowalikxm@gmail.com>
2018-02-25 22:17:45 +0100

    Changed SettingsActivity base

--- app/src/main/java/pl/appcoders/moxacontroller/settings/SettingsActivity.java
+++ app/src/main/java/pl/appcoders/moxacontroller/settings/SettingsActivity.java
@@ -1,19 +1,19 @@
 package pl.appcoders.moxacontroller.settings;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by mkowalik on 25.02.18.
  */
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        getFragmentManager().beginTransaction()
-//                .replace(android.R.id.content, new SettingsFragment())
-//                .commit();
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
     }
 }
