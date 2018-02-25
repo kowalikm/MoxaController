249eb288c362ef65dba2e5ab2a6cfdb5fe3c40cb
Mateusz Kowalik <kowalikxm@gmail.com>
2018-02-25 18:26:12 +0100

    Changed MainActivity for fragments ready

--- app/src/main/java/pl/appcoders/moxacontroller/mainactivity/MainActivity.java
+++ app/src/main/java/pl/appcoders/moxacontroller/mainactivity/MainActivity.java
@@ -3,19 +3,14 @@ package pl.appcoders.moxacontroller.mainactivity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.Optional;
-
 import pl.appcoders.moxacontroller.R;
-import pl.appcoders.moxacontroller.mainactivity.NavigationItemSelectedListener;
 
 /**
  * Created by mkowalik on 25.02.18.
@@ -49,11 +44,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
+            case R.id.menu_refresh:
+                menuRefreshHandler();
+                return true;
+            case R.id.menu_settings:
+                menuSettingsHandler();
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void menuRefreshHandler() {
+
+    }
+
+    private void menuSettingsHandler() {
+
     }
 
     private void setCustomActionBar() {
