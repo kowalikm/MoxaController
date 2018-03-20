59a1273840275f9e197754e81f18456efba80f4b
Mateusz Kowalik <kowalikxm@gmail.com>
2018-03-20 01:01:08 +0100

    Added todos

--- app/src/main/java/pl/appcoders/moxacontroller/inputs/MapInputActivity.java
+++ app/src/main/java/pl/appcoders/moxacontroller/inputs/MapInputActivity.java
@@ -11,5 +11,7 @@ public class MapInputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_input);
+        //TODO: add digital input to spinner
+        //TODO: handle insert
     }
 }
